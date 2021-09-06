46befafeaadfb6e65fee89d3cc50a5aa14dd3ab5
mma judith <mmajudith2000@gmail.com>
2021-09-06 13:31:17 -0700

    Finished creating dummy data api

--- server/model/invoiceData.js
+++ server/model/invoiceData.js
@@ -23,91 +23,107 @@ export const data = [
     },
 
     {
-        name: "John Doe",
-        email: "johndoe@serviceprovider.com",
+        name: "Jept Paul Alapat",
+        email: "Jeptpaul@serviceprovider.com",
         invoiceNo: "# INV1122",
         time: "11:35AM",
-        noOfItems: 05,
+        noOfItems: 03,
         items: [
 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
-                "Prawn & Chicken Sui Mai", 
-                "Long Product Name With Two Lines", 
-                "Long Product Name With Two Lines"
+                "Prawn & Chicken Sui Mai",
 
             ],
-        qualities: [2, 1, 3, 6, 2],
-        prices: [430, 670, 430, 1670, 270],
+        qualities: [2, 3, 2],
+        prices: [430, 1670, 270],
         subTotal: 3470,
         tax: 429,
         discount: -390,
-        grandTotal: 3509
+        grandTotal: 1240
     },
 
     {
-        name: "John Doe",
-        email: "johndoe@serviceprovider.com",
-        invoiceNo: "# INV1122",
+        name: "Unknown User",
+        email: "unknown@serviceprovider.com",
+        invoiceNo: "# INV1120",
         time: "11:35AM",
-        noOfItems: 05,
+        noOfItems: 19,
         items: [
 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
+                "Long Product Name With Two Lines",
+                "Prawn & Chicken Sui Mai", 
+                "Long Product Name With Two Lines", 
+                "Prawn & Chicken Sui Mai", 
+                "Long Product Name With Two Lines", 
+                "Long Product Name With Two Lines",
+                "Prawn & Chicken Sui Mai", 
+                "Long Product Name With Two Lines", 
+                "Prawn & Chicken Sui Mai", 
+                "Long Product Name With Two Lines", 
+                "Long Product Name With Two Lines",
+                "Prawn & Chicken Sui Mai", 
+                "Long Product Name With Two Lines", 
+                "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines"
 
             ],
-        qualities: [2, 1, 3, 6, 2],
-        prices: [430, 670, 430, 1670, 270],
+        qualities: [2, 1, 3, 6, 2, 2, 1, 3, 6, 2,2, 1, 3, 6, 2,2, 1, 3, 6],
+        prices: [430, 670, 430, 1670, 270, 430, 670, 430, 1670, 270, 430, 670, 430, 1670, 270, 430, 670, 430, 1670],
         subTotal: 3470,
         tax: 429,
         discount: -390,
-        grandTotal: 3509
+        grandTotal: 3960
     },
 
     {
-        name: "John Doe",
-        email: "johndoe@serviceprovider.com",
-        invoiceNo: "# INV1122",
+        name: "Jose Kutten baby kan...",
+        email: "jose@serviceprovider.com",
+        invoiceNo: "# INV1119",
         time: "11:35AM",
-        noOfItems: 05,
+        noOfItems: 07,
         items: [
 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
-                "Long Product Name With Two Lines"
+                "Long Product Name With Two Lines",
+                "Prawn & Chicken Sui Mai", 
+                "Long Product Name With Two Lines", 
 
             ],
-        qualities: [2, 1, 3, 6, 2],
-        prices: [430, 670, 430, 1670, 270],
+        qualities: [2, 1, 3, 6, 2, 5, 6],
+        prices: [430, 670, 430, 1670, 270, 270, 430],
         subTotal: 3470,
         tax: 429,
         discount: -390,
-        grandTotal: 3509
+        grandTotal: 2340
     },
 
     {
-        name: "John Doe",
-        email: "johndoe@serviceprovider.com",
-        invoiceNo: "# INV1122",
+        name: "Unknown User",
+        email: "unknown@serviceprovider.com",
+        invoiceNo: "# INV1118",
         time: "11:35AM",
-        noOfItems: 05,
+        noOfItems: 07,
         items: [
 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
-                "Long Product Name With Two Lines"
+                "Long Product Name With Two Lines",
+                "Prawn & Chicken Sui Mai", 
+                "Long Product Name With Two Lines", 
 
             ],
-        qualities: [2, 1, 3, 6, 2],
-        prices: [430, 670, 430, 1670, 270],
+        qualities: [2, 1, 3, 6, 2, 6, 6],
+        prices: [430, 670, 430, 1670, 270, 270, 430],
         subTotal: 3470,
         tax: 429,
         discount: -390,
@@ -115,22 +131,24 @@ export const data = [
     },
   
     {
-        name: "John Doe",
-        email: "johndoe@serviceprovider.com",
-        invoiceNo: "# INV1122",
+        name: "Unknown User",
+        email: "unknown@serviceprovider.com",
+        invoiceNo: "# INV1117",
         time: "11:35AM",
-        noOfItems: 05,
+        noOfItems: 07,
         items: [
 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
-                "Long Product Name With Two Lines"
+                "Long Product Name With Two Lines",
+                "Prawn & Chicken Sui Mai", 
+                "Long Product Name With Two Lines",
 
             ],
-        qualities: [2, 1, 3, 6, 2],
-        prices: [430, 670, 430, 1670, 270],
+        qualities: [2, 1, 3, 6, 2, 3,7],
+        prices: [430, 670, 430, 1670, 270, 270, 430],
         subTotal: 3470,
         tax: 429,
         discount: -390,
@@ -138,22 +156,24 @@ export const data = [
     },
    
     {
-        name: "John Doe",
-        email: "johndoe@serviceprovider.com",
-        invoiceNo: "# INV1122",
+        name: "John Doe sylvester stal...",
+        email: "syvester@serviceprovider.com",
+        invoiceNo: "# INV1116",
         time: "11:35AM",
-        noOfItems: 05,
+        noOfItems: 07,
         items: [
 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
-                "Long Product Name With Two Lines"
+                "Long Product Name With Two Lines",
+                "Prawn & Chicken Sui Mai", 
+                "Long Product Name With Two Lines",
 
             ],
-        qualities: [2, 1, 3, 6, 2],
-        prices: [430, 670, 430, 1670, 270],
+        qualities: [2, 1, 3, 6, 2, 4, 5],
+        prices: [430, 670, 430, 1670, 270, 270, 430],
         subTotal: 3470,
         tax: 429,
         discount: -390,
@@ -161,22 +181,24 @@ export const data = [
     },
    
     {
-        name: "John Doe",
-        email: "johndoe@serviceprovider.com",
-        invoiceNo: "# INV1122",
+        name: "Unknown User",
+        email: "unknown@serviceprovider.com",
+        invoiceNo: "# INV1116",
         time: "11:35AM",
-        noOfItems: 05,
+        noOfItems: 07,
         items: [
 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
                 "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines", 
+                "Long Product Name With Two Lines",
+                "Prawn & Chicken Sui Mai", 
                 "Long Product Name With Two Lines"
 
             ],
-        qualities: [2, 1, 3, 6, 2],
-        prices: [430, 670, 430, 1670, 270],
+        qualities: [2, 1, 3, 6, 2, 4, 7],
+        prices: [430, 670, 430, 1670, 270, 160,150],
         subTotal: 3470,
         tax: 429,
         discount: -390,
